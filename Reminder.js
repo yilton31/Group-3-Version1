320cd25baf5de1a78a3d3f99233a4474357fc26a
yilton31 <105474013+yilton31@users.noreply.github.com>
2022-06-11 17:20:41 -0400

    Add files via upload

---
+++ Reminder.js
@@ -0,0 +1,12 @@
+//variables
+const popup_window = document.querySelector('.popup');
+const sec = document.querySelector('.watch .time');
+const overlay = document.querySelector('.overlay');
+
+//conditon for popup
+
+function popup () {
+    if (sec=5)
+    display popup_window;
+
+}
